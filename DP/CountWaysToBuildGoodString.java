77be5ac118b34a88bd08466cc8df209f8dc23e69
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2024-12-31 12:27:28 +0530

    "Count ways to build good string"

---
+++ DP/CountWaysToBuildGoodString.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class CountWaysToBuildGoodString {
+    public static void main(String[] args) {
+
+    }
+
+    public static  int L , H , Z , O;
+    public static int MOD = 1_000_000_007;
+
+    public static int[] dp ;
+    public static int countGoodString(int low , int high , int zero , int one){
+
+
+
+
+        L = low ;
+        H = high;
+        Z = zero;
+        O = one;
+
+        dp = new int[H + 1];
+        Arrays.fill(dp , -1);
+
+        return solve(0);
+
+    }
+
+    public static int solve(int l){
+        if(l > H){
+            return 0;
+        }
+
+        if(dp[l] != -1){
+            return dp[l];
+        }
+
+        boolean add_1 = false;
+        if(l >= L){
+            add_1 = true;
+        }
+
+        int add_zero = solve(l + Z);
+        int add_one = solve(l + O);
+
+        return dp[l] = ((add_1 ? 0 : 1) + add_one + add_zero)%MOD;
+    }
+}
