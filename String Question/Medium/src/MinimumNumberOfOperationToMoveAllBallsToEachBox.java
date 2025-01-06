083d8ddebd37b16b88f1d79848e2d2bc7688cb98
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2025-01-06 21:40:50 +0530

    "Minimum number of operation to move all balls to each box"

--- String Question/Medium/src/MinimumNumberOfOperationToMoveAllBallsToEachBox.java
+++ String Question/Medium/src/MinimumNumberOfOperationToMoveAllBallsToEachBox.java
@@ -8,18 +8,48 @@ public class MinimumNumberOfOperationToMoveAllBallsToEachBox {
         System.out.println(Arrays.toString(minOperation(s)));
     }
 
-    public static int[] minOperation(String s){
-        int[] ans = new int[s.length()];
+//    public static int[] minOperation(String s){
+//        int[] ans = new int[s.length()];
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            int count = 0;
+//            for (int j = 0; j < s.length() ; j++) {
+//                if(s.charAt(j) == '1'){
+//                    count += Math.abs(j - i);
+//                }
+//            }
+//            ans[i] = count;
+//        }
+//        return ans;
+//    }
 
-        for (int i = 0; i < s.length(); i++) {
-            int count = 0;
-            for (int j = 0; j < s.length() ; j++) {
-                if(s.charAt(j) == '1'){
-                    count += Math.abs(j - i);
-                }
-            }
-            ans[i] = count;
+   public static int[] minOperation(String boxes){
+        int n = boxes.length();
+
+        int cumValue = 0;
+        int cumValueSum = 0;
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = cumValueSum;
+
+            cumValue += boxes.charAt(i) == '0' ? 0 : 1;
+
+            cumValueSum += cumValue;
         }
+
+        cumValue = 0;
+        cumValueSum = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] += cumValueSum;
+
+            cumValue += boxes.charAt(i) == '0' ? 0 : 1;
+
+            cumValueSum += cumValue;
+        }
+
         return ans;
     }
 }
