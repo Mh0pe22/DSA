7c59260bc11bc47661727b65a47716fa1684ecd7
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2025-02-07 00:09:24 +0530

    "Tuples with same product"

--- Array/src/TupleWithSameProduct.java
+++ Array/src/TupleWithSameProduct.java
@@ -3,7 +3,8 @@ import java.util.Map;
 
 public class TupleWithSameProduct {
     public static void main(String[] args) {
-
+        int[] nums = {2 , 3, 4, 6};
+        System.out.println(product(nums));
     }
 
     public static int product(int[] nums){
@@ -13,7 +14,14 @@ public class TupleWithSameProduct {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 int p = nums[i] * nums[j];
+                mp.put(p , mp.getOrDefault(p , 0) + 1);
             }
         }
+
+        for(int freq : mp.values()){
+            tuples += (freq * (freq - 1)) / 2;
+        }
+
+        return tuples * 8;
     }
 }
